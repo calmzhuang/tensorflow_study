fd06097d4754a5f76c4dd2c2a3140c7fc4ca9110
calmzhuang <calmzhuang@163.com>
2019-05-04 21:36:40 +0800

    fixed seq2seq_app

--- nine_chapter/three_section/seq_to_seq_app.py
+++ nine_chapter/three_section/seq_to_seq_app.py
@@ -1,9 +1,6 @@
 import tensorflow as tf
-import codecs
-import sys
 
-checkpoint_path = "./seq2seq_checkpoint/seq2seq_ckpt-9000"
-# checkpoint_path = "./seq2seq_ckpt-9000"
+checkpoint_path = "./seq2seq_checkpoint/seq2seq_ckpt-3800"
 
 hidden_size = 1024
 num_layers = 2
@@ -13,9 +10,6 @@ share_emd_and_softmax = True
 
 sos_id = 1
 eos_id = 2
-
-src_vocab_file = "./ptb-en.vocab"
-trg_vocab_file = "./ptb-zh.vocab"
 
 
 class NmtModel(object):
@@ -66,7 +60,7 @@ class NmtModel(object):
 
             def loop_body(state, trg_ids, step):
                 trg_input = [trg_ids.read(step)]
-                trg_emb = tf.nn.embedding_lookup(self.trg_embedding,trg_input)
+                trg_emb = tf.nn.embedding_lookup(self.trg_embedding, trg_input)
                 dec_outputs, next_state = self.dec_cell.call(state=state, inputs=trg_emb)
                 output = tf.reshape(dec_outputs, [-1, hidden_size])
                 logits = (tf.matmul(output, self.softmax_weight) + self.softmax_bias)
@@ -79,39 +73,17 @@ class NmtModel(object):
 
 
 def main():
-    # 定义训练用的循环神经网络模型。
     with tf.variable_scope("nmt_model", reuse=None):
         model = NmtModel()
+    test_sentence = [90, 13, 9, 689, 4, 2]
 
-    # 定义个测试句子。
-    test_en_text = "This is my son . <eos>"
-    print(test_en_text)
-
-    # 根据英文词汇表，将测试句子转为单词ID。
-    with codecs.open(src_vocab_file, "r", "utf-8") as f_vocab:
-        src_vocab = [w.strip() for w in f_vocab.readlines()]
-        src_id_dict = dict((src_vocab[x], x) for x in range(len(src_vocab)))
-    test_en_ids = [(src_id_dict[token] if token in src_id_dict else src_id_dict['<unk>'])
-                   for token in test_en_text.split()]
-    print(test_en_ids)
-
-    # 建立解码所需的计算图。
-    output_op = model.inference(test_en_ids)
+    output_op = model.inference(test_sentence)
     sess = tf.Session()
     saver = tf.train.Saver()
     saver.restore(sess, checkpoint_path)
 
-    # 读取翻译结果。
-    output_ids = sess.run(output_op)
-    print(output_ids)
-
-    # 根据中文词汇表，将翻译结果转换为中文文字。
-    with codecs.open(trg_vocab_file, "r", "utf-8") as f_vocab:
-        trg_vocab = [w.strip() for w in f_vocab.readlines()]
-    output_text = ''.join([trg_vocab[x] for x in output_ids])
-
-    # 输出翻译结果。
-    print(output_text.encode('utf8').decode(sys.stdout.encoding))
+    output = sess.run(output_op)
+    print(output)
     sess.close()
 
 
